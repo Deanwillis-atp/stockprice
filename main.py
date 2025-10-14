4b3a9cbdc6e13b07bcafad132d140130febaed47
Dean Willis <deanwillis@outlook.com>
2025-10-14 17:06:59 -0400

    simple use of yfinance

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello word")
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import yfinance as yf #yahoo finance 
+
+msft = yf.Ticker("MSFT")
+info = msft.info
+history = msft.history(period="max")
+split_dividends = msft.actions
+
+print(split_dividends)
